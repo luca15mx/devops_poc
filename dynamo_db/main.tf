87a4ea89cd4ac638b85bcd34bc7f800b58015bef
Mario Roberto <mariohdzflores@gmail.com>
2021-06-23 00:42:55 -0500

    Crete DynoDB Table

--- dynamo_db/main.tf
+++ dynamo_db/main.tf
@@ -1,141 +1,45 @@
-locals {
-  enabled = module.this.enabled
+# #################################################################################
+# Creates POC Lambda-DynamoDB-ApiGateway Reader
+#
+# MRHF 22-JUN-2022 23:38
+#
+# #################################################################################
 
-  attributes = concat(
-    [
-      {
-        name = var.range_key
-        type = var.range_key_type
-      },
-      {
-        name = var.hash_key
-        type = var.hash_key_type
-      }
-    ],
-    var.dynamodb_attributes
-  )
+# #################################################################################
+#  Creates the DynamoDB Table
+#
+# #################################################################################
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = var.mdyn_db_db_name
+  billing_mode   = var.mdyn_db_billing_mode
+  write_capacity = var.mdyn_db_write_capacity
+  read_capacity  = var.mdyn_db_read_capacity
+  hash_key       = var.mdyn_db_hash_key
 
-  # Remove the first map from the list if no `range_key` is provided
-  from_index = length(var.range_key) > 0 ? 0 : 1
-
-  attributes_final = slice(local.attributes, local.from_index, length(local.attributes))
-}
-
-resource "null_resource" "global_secondary_index_names" {
-  count = (local.enabled ? 1 : 0) * length(var.global_secondary_index_map)
-
-  # Convert the multi-item `global_secondary_index_map` into a simple `map` with just one item `name` since `triggers` does not support `lists` in `maps` (which are used in `non_key_attributes`)
-  # See `examples/complete`
-  # https://www.terraform.io/docs/providers/aws/r/dynamodb_table.html#non_key_attributes-1
-  triggers = {
-    "name" = var.global_secondary_index_map[count.index]["name"]
-  }
-}
-
-resource "null_resource" "local_secondary_index_names" {
-  count = (local.enabled ? 1 : 0) * length(var.local_secondary_index_map)
-
-  # Convert the multi-item `local_secondary_index_map` into a simple `map` with just one item `name` since `triggers` does not support `lists` in `maps` (which are used in `non_key_attributes`)
-  # See `examples/complete`
-  # https://www.terraform.io/docs/providers/aws/r/dynamodb_table.html#non_key_attributes-1
-  triggers = {
-    "name" = var.local_secondary_index_map[count.index]["name"]
-  }
-}
-
-resource "aws_dynamodb_table" "default" {
-  count            = local.enabled ? 1 : 0
-  name             = module.this.id
-  billing_mode     = var.billing_mode
-  read_capacity    = var.autoscale_min_read_capacity
-  write_capacity   = var.autoscale_min_write_capacity
-  hash_key         = var.hash_key
-  range_key        = var.range_key
-  stream_enabled   = length(var.replicas) > 0 ? true : var.enable_streams
-  stream_view_type = length(var.replicas) > 0 || var.enable_streams ? var.stream_view_type : ""
-
-
-  server_side_encryption {
-    enabled     = var.enable_encryption
-    kms_key_arn = var.server_side_encryption_kms_key_arn
+  attribute {
+    name = "ID"
+    type = "N"
   }
 
-  point_in_time_recovery {
-    enabled = var.enable_point_in_time_recovery
+  attribute {
+    name = "Word"
+    type = "S"
   }
 
-  lifecycle {
-    ignore_changes = [
-      read_capacity,
-      write_capacity
-    ]
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
   }
 
-  dynamic "attribute" {
-    for_each = local.attributes_final
-    content {
-      name = attribute.value.name
-      type = attribute.value.type
-    }
+  global_secondary_index {
+    name               = "WordIndex"
+    hash_key           = "ID"
+    range_key          = "Word"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["Word"]
   }
 
-  dynamic "global_secondary_index" {
-    for_each = var.global_secondary_index_map
-    content {
-      hash_key           = global_secondary_index.value.hash_key
-      name               = global_secondary_index.value.name
-      non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", null)
-      projection_type    = global_secondary_index.value.projection_type
-      range_key          = lookup(global_secondary_index.value, "range_key", null)
-      read_capacity      = lookup(global_secondary_index.value, "read_capacity", null)
-      write_capacity     = lookup(global_secondary_index.value, "write_capacity", null)
-    }
-  }
-
-  dynamic "local_secondary_index" {
-    for_each = var.local_secondary_index_map
-    content {
-      name               = local_secondary_index.value.name
-      non_key_attributes = lookup(local_secondary_index.value, "non_key_attributes", null)
-      projection_type    = local_secondary_index.value.projection_type
-      range_key          = local_secondary_index.value.range_key
-    }
-  }
-
-  dynamic "replica" {
-    for_each = var.replicas
-    content {
-      region_name = replica.value
-    }
-  }
-
-  dynamic "ttl" {
-    for_each = var.ttl_enabled ? [1] : []
-    content {
-      attribute_name = var.ttl_attribute
-      enabled        = var.ttl_enabled
-    }
-  }
-
-  tags = module.this.tags
-}
-
-module "dynamodb_autoscaler" {
-  source  = "cloudposse/dynamodb-autoscaler/aws"
-  version = "0.13.1"
-  enabled = local.enabled && var.enable_autoscaler && var.billing_mode == "PROVISIONED"
-
-  attributes                   = concat(module.this.attributes, var.autoscaler_attributes)
-  tags                         = merge(module.this.tags, var.autoscaler_tags)
-  dynamodb_table_name          = join("", aws_dynamodb_table.default.*.id)
-  dynamodb_table_arn           = join("", aws_dynamodb_table.default.*.arn)
-  dynamodb_indexes             = null_resource.global_secondary_index_names.*.triggers.name
-  autoscale_write_target       = var.autoscale_write_target
-  autoscale_read_target        = var.autoscale_read_target
-  autoscale_min_read_capacity  = var.autoscale_min_read_capacity
-  autoscale_max_read_capacity  = var.autoscale_max_read_capacity
-  autoscale_min_write_capacity = var.autoscale_min_write_capacity
-  autoscale_max_write_capacity = var.autoscale_max_write_capacity
-
-  context = module.this.context
+  tags = var.mdyn_db_tags
 }
--- dynamo_db/variables.tf
+++ dynamo_db/variables.tf
@@ -1,164 +1,44 @@
-variable "autoscale_write_target" {
-  type        = number
-  default     = 50
-  description = "The target value (in %) for DynamoDB write autoscaling"
+# #################################################################################
+# Creates POC Lambda-DynamoDB-ApiGateway Reader
+#
+# MRHF 22-JUN-2022 23:38
+#
+# #################################################################################
+
+# #################################################################################
+# Variables for DynamoDB Module
+#
+# #################################################################################
+
+variable "mdyn_db_db_name" {
+  type = string
 }
 
-variable "autoscale_read_target" {
-  type        = number
-  default     = 50
-  description = "The target value (in %) for DynamoDB read autoscaling"
+variable "mdyn_db_billing_mode" {
+  type = string
+  # default = "PROVISIONED"
 }
 
-variable "autoscale_min_read_capacity" {
-  type        = number
-  default     = 5
-  description = "DynamoDB autoscaling min read capacity"
+variable "mdyn_db_read_capacity" {
+  type = number
+  # default = 20
 }
 
-variable "autoscale_max_read_capacity" {
-  type        = number
-  default     = 20
-  description = "DynamoDB autoscaling max read capacity"
+variable "mdyn_db_write_capacity" {
+  type = number
+  # default = 20
 }
 
-variable "autoscale_min_write_capacity" {
-  type        = number
-  default     = 5
-  description = "DynamoDB autoscaling min write capacity"
+variable "mdyn_db_hash_key" {
+  type = string
+  # default = "ID"
 }
 
-variable "autoscale_max_write_capacity" {
-  type        = number
-  default     = 20
-  description = "DynamoDB autoscaling max write capacity"
+variable "mdyn_db_range_key" {
+  type = string
+  # default = "word"
 }
 
-variable "billing_mode" {
-  type        = string
-  default     = "PROVISIONED"
-  description = "DynamoDB Billing mode. Can be PROVISIONED or PAY_PER_REQUEST"
-}
-
-variable "enable_streams" {
-  type        = bool
-  default     = false
-  description = "Enable DynamoDB streams"
-}
-
-variable "stream_view_type" {
-  type        = string
-  default     = ""
-  description = "When an item in the table is modified, what information is written to the stream"
-}
-
-variable "enable_encryption" {
-  type        = bool
-  default     = true
-  description = "Enable DynamoDB server-side encryption"
-}
-
-variable "server_side_encryption_kms_key_arn" {
-  type        = string
-  default     = null
-  description = "The ARN of the CMK that should be used for the AWS KMS encryption. This attribute should only be specified if the key is different from the default DynamoDB CMK, alias/aws/dynamodb."
-}
-
-variable "enable_point_in_time_recovery" {
-  type        = bool
-  default     = true
-  description = "Enable DynamoDB point in time recovery"
-}
-
-variable "hash_key" {
-  type        = string
-  description = "DynamoDB table Hash Key"
-}
-
-variable "hash_key_type" {
-  type        = string
-  default     = "S"
-  description = "Hash Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
-}
-
-variable "range_key" {
-  type        = string
-  default     = ""
-  description = "DynamoDB table Range Key"
-}
-
-variable "range_key_type" {
-  type        = string
-  default     = "S"
-  description = "Range Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
-}
-
-variable "ttl_attribute" {
-  type        = string
-  default     = "Expires"
-  description = "DynamoDB table TTL attribute"
-}
-
-variable "ttl_enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to disable DynamoDB table TTL"
-}
-
-variable "enable_autoscaler" {
-  type        = bool
-  default     = false
-  description = "Flag to enable/disable DynamoDB autoscaling"
-}
-
-variable "autoscaler_attributes" {
-  type        = list(string)
-  default     = []
-  description = "Additional attributes for the autoscaler module"
-}
-
-variable "autoscaler_tags" {
-  type        = map(string)
-  default     = {}
-  description = "Additional resource tags for the autoscaler module"
-}
-
-variable "dynamodb_attributes" {
-  type = list(object({
-    name = string
-    type = string
-  }))
-  default     = []
-  description = "Additional DynamoDB attributes in the form of a list of mapped values"
-}
-
-variable "global_secondary_index_map" {
-  type = list(object({
-    hash_key           = string
-    name               = string
-    non_key_attributes = list(string)
-    projection_type    = string
-    range_key          = string
-    read_capacity      = number
-    write_capacity     = number
-  }))
-  default     = []
-  description = "Additional global secondary indexes in the form of a list of mapped values"
-}
-
-variable "local_secondary_index_map" {
-  type = list(object({
-    name               = string
-    non_key_attributes = list(string)
-    projection_type    = string
-    range_key          = string
-  }))
-  default     = []
-  description = "Additional local secondary indexes in the form of a list of mapped values"
-}
-
-variable "replicas" {
-  type        = list(string)
-  default     = []
-  description = "List of regions to create replica"
+variable "mdyn_db_tags" {
+  type = map
 }
