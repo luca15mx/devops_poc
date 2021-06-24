4c7a5d52a3f25d322b010c7c6ac09b5f7e9a09f4
Mario Roberto <mariohdzflores@gmail.com>
2021-06-24 12:39:29 -0500

    Fixes 25 06 2021

--- lambda/main_lambda.tf
+++ lambda/main_lambda.tf
@@ -1,16 +1,10 @@
-# # IAM role which dictates what other AWS services the Lambda function
-# # may access.
-# resource "aws_iam_role_policy" "lambda_policy" {
-#   name   = "lambda_policy"
-#   role   = aws_iam_role.lambda_exec.id
-#   policy = file("./lambda/policies/iam_cwlogs_for_lambda.json")
-# }
+################################################################################
 #
-# resource "aws_iam_role" "lambda_exec" {
-#   name               = "lambda_exec_role"
-#   assume_role_policy = file("./lambda/policies/iam_assume_policy_lambda_exec.json")
-#   tags               = var.mlambda_common_tags
-# }
+#  Module to upload and create the Lambda function.
+#
+#  MHRF 23/06/2021  14:50
+#
+################################################################################
 
 resource "aws_lambda_function" "http_crud_lambda" {
   function_name = var.mlambda_function_name
@@ -18,7 +12,6 @@ resource "aws_lambda_function" "http_crud_lambda" {
   s3_key        = var.mlambda_s3_code_key_file
   handler       = var.mlambda_handler
   runtime       = var.mlambda_runtime
-  # role          = aws_iam_role.lambda_exec.arn
-  role = var.mlambda_iam_exec_role_arn
-  tags = var.mlambda_common_tags
+  role          = var.mlambda_iam_exec_role_arn
+  tags          = var.mlambda_common_tags
 }
